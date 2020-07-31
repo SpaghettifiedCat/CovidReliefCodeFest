7aa77dd9abff48c144c9009609d584bb84b674be
SpaghettifiedCat <khandanish2004@gmail.com>
2020-07-31 21:17:11 +0530

    Deleting Files

--- meow.py
+++
@@ -1,9 +0,0 @@
-import random
-cases = []
-test_cases = input('Number of cases?: ')
-for i in range(int(test_cases)):
-    cases.append(input('Number of distinct cells: '))
-king_x = random.randint(1, 8)
-king_y = random.randint(1, 8)
-king = (king_x, king_y)
-print(king)
