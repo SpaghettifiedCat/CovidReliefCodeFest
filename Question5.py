221f00752561a1720e93e71dbe98ab84990d1847
SpaghettifiedCat <khandanish2004@gmail.com>
2020-08-01 17:38:58 +0530

    Upload solutions

--- Question5.py
+++ Question5.py
@@ -5,17 +5,39 @@ Created on Sat Aug  1 15:22:22 2020
 
 @author: spaghettifiedcat
 """
-def divisor(n):
-  for i in range(n):
-    x = len([i for i in range(1,n+1) if not n % i])
-  return x
-def triangle_number_generator(n):
-    return((n*(n+1))/2)
-m = 1
-divisors = 0
-while divisors < 200:
-    triangle_number = triangle_number_generator(m)
-    divisors = divisor(m)
-    print(str(triangle_number) + ' ' + str(divisors))
-    m += 1
-print("The triangluar number is: " + str(triangle_number))
+
+def triangle_num_generator():
+    n = 1
+    s = 0
+    while 1:
+        s += n
+        n += 1
+        yield s
+
+
+def triangle_num_naive(n):
+    tgen = triangle_num_generator()
+    ret = 0
+    for i in range(n):
+        ret = next(tgen)
+    return ret
+def divisor_gen(n):
+    divisors = []
+    for i in range(1, int(n**0.5) + 1):
+        if n % i == 0:
+            yield i
+            if i is not n / i:
+                divisors.insert(0, n / i)
+    for div in divisors:
+        yield div
+def divisors(n):
+    return [d for d in divisor_gen(n)]
+num_divs = 0
+i = 1
+while num_divs < 200:
+    i += 1
+    tnum = triangle_num_naive(i)
+    divs = divisors(tnum)
+    num_divs = len(divs)
+
+print(tnum) 
